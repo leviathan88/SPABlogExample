d4b490ab4d685396bd7633647e86124772c9256f
Elvis <elvisdedic@outlook.com>
2015-06-07 16:31:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,15 +2,12 @@
  var app = express();
  var path = require('path');
  var port = process.env.PORT || 4000;
-
- // set the public folder to serve public assets
+ 
  app.use(express.static(__dirname + '/public'));
-
- // set up our one route to the index.html file
+ 
  app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname + '/public/views/index.html'));
  });
-
- // start the server on port 8080 (http://localhost:8080)
+ 
  app.listen(port);
  console.log('It has begun');
